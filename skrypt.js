f00da372f92ab4863fee08abac67e6e42eb20261
zadaraEI19 <102586149+zadaraEI19@users.noreply.github.com>
2023-02-24 10:39:45 +0100

    Update skrypt.js

--- skrypt.js
+++ skrypt.js
@@ -1,1 +1,18 @@
+const mysql = require('mysql2');
+async function main() {
+	var connection = mysql.createConnection({
+  	host     : 'localhost',
+  	user     : 'matiz',
+  	password : '1lx8jXEPqBYx!0cI',
+  	database : 'projekt3_matiz'
+	});
+ 
+	connection.connect();
+    
+    responses = [];
 
+    let zapytanie1 = await a.promise().query("INSERT INTO Książki (ISBN,Ewidencyjny,Tytul,Wydawca)  VALUE (",document.forms.formularz.isbn.value,",",document.forms.formularz.ewident1.value,",",document.forms.formularz.tytul.value,",",document.forms.formularz.wydawca.value,")");
+	responses.push(q2);
+
+	connection.end();
+}
