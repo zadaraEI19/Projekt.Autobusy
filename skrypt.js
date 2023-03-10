8b2a8b915e070d06188d42c9b1e008e030f1caa2
zadaraEI19 <102586149+zadaraEI19@users.noreply.github.com>
2023-03-10 18:08:06 +0100

    bez licznika na zywo

--- skrypt.js
+++ skrypt.js
@@ -1,18 +1,20 @@
-const mysql = require('mysql2');
-async function main() {
-	var connection = mysql.createConnection({
-  	host     : 'localhost',
-  	user     : 'matiz',
-  	password : '1lx8jXEPqBYx!0cI',
-  	database : 'projekt3_matiz'
-	});
- 
-	connection.connect();
-    
-    responses = [];
-
-    let zapytanie1 = await a.promise().query("INSERT INTO Książki (ISBN,Ewidencyjny,Tytul,Wydawca)  VALUE (",document.forms.formularz.isbn.value,",",document.forms.formularz.ewident1.value,",",document.forms.formularz.tytul.value,",",document.forms.formularz.wydawca.value,")");
-	responses.push(q2);
-
-	connection.end();
+function zmiana() {
+    document.getElementById("odjazd1").innerHTML = "";
+    if(document.forms.formularz.przystaneks.value == "Centrum") {
+        document.getElementById("odjazd1").innerHTML = '<label for="przystanekk">Przystanek końcowy:</label><select onchange="bilet()" name="przystanekk" id="przystaneks"><option>Rogatka</option><option>Bojszow</option><option>Stadion</option></select>';
+        document.getElementById("odjazd2").innerHTML = '<select name="odjazdd" id="odjazd_c"><option>9:00</option><option>11:00</option><option>14:00</option></select>';
+    }
+    else if(document.forms.formularz.przystaneks.value == "Rogatka") {
+        document.getElementById("odjazd1").innerHTML = '<label for="przystanekk">Przystanek końcowy:</label><select onchange="bilet()" name="przystanekk" id="przystaneks"><option>Bojszow</option><option>Stadion</option></select>';
+        document.getElementById("odjazd2").innerHTML = '<select name="odjazdd" id="odjazd_r"><option>9:15</option><option>11:15</option><option>14:15</option></select>';
+    }
+    else if(document.forms.formularz.przystaneks.value == "Bojszow") {
+        document.getElementById("odjazd1").innerHTML = '<label for="przystanekk">Przystanek końcowy:</label><select onchange="bilet()" name="przystanekk" id="przystaneks"><option>Stadion</option></select>';
+        document.getElementById("odjazd2").innerHTML = '<select name="odjazdd" id="odjazd_b"><option>9:40</option><option>11:40</option><option>14:40</option></select>';
+    }
+    else if(document.forms.formularz.przystaneks.value == "Stadion") {
+        document.getElementById("odjazd1").innerHTML = 'To jest ostatni przystanek!';
+        document.getElementById("odjazd2").innerHTML = "";
+    }
 }
+
