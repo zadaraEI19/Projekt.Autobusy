53fe774bbf5bd060d128fd3dd44d1c2a69c19ea0
zadaraEI19 <102586149+zadaraEI19@users.noreply.github.com>
2023-03-30 18:15:19 +0200

    teraz działa

--- opinia.php
+++ opinia.php
@@ -1,5 +1,7 @@
 <?php
-$link1 = @mysqli_connect("localhost","root","","test");
-$query = "insert into oceny_jazdy (id_busa, ocena) VALUES(1,'"$_POST["opinia"]"')";
+$link1 = @mysqli_connect("localhost","root","","projekt_autobusy");
+$query = "insert into oceny_jazdy (id_busa, ocena) VALUES(1,'".$_POST["opinia"]."')";
 $result = mysqli_query($link1, $query);
+
+header("location:index.html");
 ?>
